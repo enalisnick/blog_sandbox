c1dff1e32ceec619eb1b13e46cf5d8fc2a15f502
enalisnick <enalisnick@gmail.com>
2016-12-04 19:57:36 -0800

    added parametric approximations

--- reference_priors/betaSimulation.py
+++ reference_priors/betaSimulation.py
@@ -6,7 +6,13 @@ from scipy.stats import beta
 def logistic(x):
     return 1./(1+np.exp(-x))
 
-def compute_ref_approx(t=10, m=100, s=100, lr=0.01, rs=1000):
+def softplus(x):
+    return np.log(1+np.exp(x))
+
+def kumaraswamyPDF(x, a,b):
+    return a*b * (x**(a-1)) * (1-(x**a))**(b-1)
+
+def compute_nonParam_ref_approx(t=50, m=200, s=500, lr=0.0001, rs=1000):
     w = np.random.normal(size=(10,1))
     b = 0.
     for t_idx in xrange(t):
@@ -49,6 +55,48 @@ def compute_ref_approx(t=10, m=100, s=100, lr=0.01, rs=1000):
     return [logistic(np.dot(np.random.normal(size=(1,10)),w)+b)[0,0] for k in xrange(rs)]
 
 
+def compute_param_ref_approx(t=25, m=200, s=500, lr=0.0001):
+    a_w = np.random.normal()
+    b_w = np.random.normal()
+
+    for t_idx in xrange(t):
+        ### term 1                                                                                                                                                      
+        # compute p_hat                                                                                                         
+        epsilon = np.random.uniform()
+        a = softplus(a_w)
+        b = softplus(b_w)
+        theta_hat = (1.-(epsilon**(1./b)))**(1./a)
+
+        # sample data                                                                                                                                       
+        Us = np.random.uniform(0,1,size=(1,m))
+        Ls = np.log(Us) - np.log(1-Us)
+        alpha = -np.log(1-theta_hat)
+        data = logistic(Ls + np.log(alpha))
+
+        ### term2                                                                                                                                                       
+        # compute p_hat k's                                                                                                                                            
+        epsilon_ks = [np.random.uniform() for s_idx in xrange(s)]
+        theta_hat_ks = [ (1.-(epsilon_k**(1./b)))**(1./a) for epsilon_k in epsilon_ks]
+
+        ### update params                                                                                                                                               
+        # term1                                                                                                                                                        
+        dData_dp = np.sum(data * (1-data)) * (-1./((1-theta_hat)*np.log(1-theta_hat)))
+        dTerm1_dp = dData_dp*np.log(theta_hat) + np.sum(data)/theta_hat + (m - dData_dp)*np.log(1-theta_hat) + -(m - np.sum(data))/(1-theta_hat)
+
+        # term2                                                                                                                                         
+        dTerm2_dp = [dData_dp*np.log(theta_k) + np.sum(data)/theta_k + (m - dData_dp)*np.log(1-theta_k) + -(m - np.sum(data))/(1-theta_k) for theta_k in theta_hat_ks]
+
+        dp0_da = theta_hat * np.log(1. - (epsilon**(1./b))) * (-1./(a**2))
+        dp0_db = (1./a) * (1. - (epsilon**(1./b)))**(1./a - 1.) * -(epsilon**(1./b)) * np.log(epsilon) * (-1./(b**2))
+        dpk_da = [theta_hat_ks[k] * np.log(1. - (epsilon_ks[k]**(1./b))) * (-1./(a**2)) for k in xrange(s)]
+        dpk_db = [(1./a) * (1. - (epsilon_ks[k]**(1./b)))**(1./a - 1.) * -(epsilon_ks[k]**(1./b)) * np.log(epsilon_ks[k]) * (-1./(b**2)) for k in xrange(s)]
+
+        a_w += lr * (dTerm1_dp*dp0_da*logistic(a_w) - np.mean([dTerm2_dp[k]*dpk_da[k]*logistic(a_w) for k in xrange(s)]))
+        b_w += lr * (dTerm1_dp*dp0_db*logistic(b_w) - np.mean([dTerm2_dp[k]*dpk_db[k]*logistic(b_w) for k in xrange(s)]))
+
+    return softplus(a_w), softplus(b_w)
+
+
 def bergerAlg(K=10, k2=500, M=500, J=100):
     prior_p = []
     prior_v = []
@@ -80,16 +128,19 @@ if __name__ == '__main__':
 
     y = beta.pdf(x, .5, .5)
     x_berger, y_berger = bergerAlg()
-    y_approxRef = compute_ref_approx(rs=1000)
+    y_approxRef = compute_nonParam_ref_approx(rs=1000)
+    kumar_a, kumar_b = compute_param_ref_approx()
+    y_param_approxRef = kumaraswamyPDF(x, kumar_a, kumar_b)
     
     plt.figure()
-    plt.plot(x, y, 'b-', linewidth=5, label="Jeffreys Prior, Beta(.5,.5)")
-    plt.plot(x_berger, y_berger, 'rx--', markersize=10, mew=5, linewidth=5, label="Brute Force Algorithm")
-    n, bins, patches = plt.hist(y_approxRef, facecolor="k", normed=True, stacked=True, alpha=.5, bins=np.arange(0, 1.1, .05), edgecolor="none", label="Linear Model Approx.")
+    plt.plot(x, y, 'k--', linewidth=5, label="Jeffreys Prior Beta(.5,.5)")
+    plt.plot(x, y_param_approxRef, 'g-', linewidth=5, label=r"Param Approx, Kumaraswamy($a=$%.2f, $b=$%.2f)"%(kumar_a,kumar_b))
+    plt.plot(x_berger, y_berger, 'rx--', markersize=10, mew=5, linewidth=3, label="Brute Force Algorithm")
+    n, bins, patches = plt.hist(y_approxRef, facecolor="b", normed=True, stacked=True, alpha=.6, bins=np.arange(0, 1.1, .05), edgecolor="none", label="Non-Param Approx, Linear Model")
     plt.xlim([0,1])
     plt.ylim([0,5])
     plt.title("Reference Priors for Bernoulli Parameter")
-    plt.xlabel(r"Bernoulli Parameter $p$")
+    plt.xlabel(r"$p$")
     plt.ylabel(r"$p^{*}(p)$")
     plt.legend()
     plt.show()
--- reference_priors/gaussianSimulation.py
+++ reference_priors/gaussianSimulation.py
@@ -8,10 +8,13 @@ def softplus(x):
 def logistic(x):
     return 1./(1+np.exp(-x))
 
+def exponentialPDF(x, rate):
+    return rate * np.exp(-rate*x)
+
 def gaussJeffPrior(x):
     return 1./x
 
-def compute_ref_approx(t=20, s=100, lr=0.1, rs=1000):
+def compute_nonParam_ref_approx(t=20, s=100, lr=0.1, rs=1000):
     w = np.random.normal(size=(10,1))
     b = 0.
     for t_idx in xrange(t):
@@ -33,6 +36,25 @@ def compute_ref_approx(t=20, s=100, lr=0.1, rs=1000):
         b += lr * (-1./theta_hat * logistic(a) * 1 + np.mean(neg_b_grad))
 
     return [softplus(np.dot(np.random.normal(size=(1,10)),w)+b) for k in xrange(rs)]
+
+
+def compute_param_ref_approx(t=20, s=100, lr=0.1):
+    w = np.random.normal()
+
+    for t_idx in xrange(t):
+        # term 1                                                                                                                                                                          
+        epsilon = np.random.uniform(0,1)
+        theta_hat = -np.log(epsilon)/softplus(w)
+
+        # term2                                                                                                                                                                          
+        epsilon_ks = [np.random.uniform(0,1) for s_idx in xrange(s)]
+        theta_hat_ks = [-np.log(epsilon_k)/softplus(w) for epsilon_k in epsilon_ks]
+
+        # update params                                                                                                                                                                
+        neg_w_grad = [(-1./theta_hat_ks[k] * np.log(epsilon_ks)/softplus(w)**2 * logistic(w)) for k in xrange(s)]
+        w += lr * (-1./theta_hat * np.log(epsilon)/softplus(w)**2 * logistic(w) + np.mean(neg_w_grad))
+
+    return softplus(w)
 
 
 def bergerAlg(K=10, k2=500, M=500, J=100):
@@ -65,17 +87,20 @@ if __name__=='__main__':
     
     x = np.linspace(.00001, 5, 500)
     
-    y_jeff = gaussJeffPrior(x)
-    y_approxRef = compute_ref_approx(rs=1000)
+    y_jeff = gaussJeffPrior(x)/4.
+    y_approxRef = compute_nonParam_ref_approx(rs=1000)
+    rate = compute_param_ref_approx()
+    y_param_approxRef = exponentialPDF(x, rate)
     x_berger, y_berger = bergerAlg()
-    
     sigma_bins = [z for z in np.linspace(0,10,20)]
 
     plt.figure()
-    plt.plot(x, y_jeff, 'b-', linewidth=5, label=r"Jeffreys Prior $1/\sigma$")
-    plt.plot(x_berger, y_berger, 'rx--', markersize=10, mew=5, linewidth=5, label="Brute Force Algorithm")
-    n, bins, patches = plt.hist(y_approxRef, facecolor="k", normed=True, stacked=True, alpha=.5, bins=np.arange(0, 5.1, .1), edgecolor="none", label="Linear Model Approx.")
-    plt.ylim(-.01,6)
+    n, bins, patches = plt.hist(y_approxRef, facecolor="b", normed=True, stacked=True, alpha=.65, bins=np.arange(0, 5.1, .1), edgecolor="none", label="Non-Param Approx, Linear Model")
+    plt.plot(x, y_param_approxRef, 'g-', linewidth=5, label=r"Param Approx, Exp($\lambda=$%.1f)"%(rate))
+    plt.plot(x, y_jeff, 'k--', linewidth=5, label=r"Jeffreys Prior $1/4\sigma$")
+    plt.plot(x_berger, y_berger, 'rx--', markersize=10, mew=5, linewidth=3, label="Brute Force Algorithm")
+
+    plt.ylim(0,4)
     plt.xlim(0,5)
     plt.legend()
     plt.title("Reference Priors for Gaussian Scale Parameter")
